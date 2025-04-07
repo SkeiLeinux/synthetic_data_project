dcba698859733f223f890b599f71c8e346dc295a
SkeiLeinux <skyline987650@gmail.com>
2025-04-07 10:21:53 +0300

    добавление минимального логирования

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from data_manager import DataManager
 from generator import generate_synthetic_data
 from processor import DataProcessor
+from logger_config import setup_logger
+
+
+logger = setup_logger(__name__)
+logger.info("Старт программы")
 
 
 def main():
@@ -19,6 +24,7 @@ def main():
     # Загрузка данных из БД
     query = "SELECT * FROM synthetic_data_schema.raw_data;"
     original_df = dm.load_data(query)
+    logger.info(f"Загружено {len(original_df)} строк из базы")
 
     print("🗃️ Исходные данные:")
     print(original_df.head())
@@ -26,12 +32,13 @@ def main():
     # Предобработка данных
     processor = DataProcessor(original_df)
     processed_df = processor.preprocess()
-
+    logger.info(f"Выполнена предобработка данных")
     print("\n🛠️ Данные после предобработки:")
     print(processed_df.head())
 
     # Основная статистика
     stats = processor.basic_statistics()
+    logger.info(f"Статистика исходных данных: \n {stats}")
     print("\n📊 Статистика исходных данных:")
     print(stats)
 
@@ -42,22 +49,26 @@ def main():
         'column_date': {'days': 3},  # дата
         'column_timestamp': {'minutes': 60},  # дата-время (таймстамп)
     }
-
+    logger.info(f"Допустимые отклонения: \n {tolerances}")
     synthetic_df = generate_synthetic_data(processed_df)
 
     stats_ok, violations = processor.compare_statistics(synthetic_df, tolerances)
 
     if stats_ok:
         print("✅ Статистики в допустимых пределах.")
+        logger.info(f"✅ Статистики в допустимых пределах.")
     else:
         print("⚠️ Нарушения статистик:")
+        logger.info(f":⚠️ Нарушения статистик:")
         for key, msg in violations.items():
-            print(123123123123123123123)
+            logger.info(f":\n {key}: {msg}")
             print(f"{key}: {msg}")
 
     print(synthetic_df)
 
     dm.close()
+
+    logger.info(f":Работа завершена")
 
 
 if __name__ == "__main__":
